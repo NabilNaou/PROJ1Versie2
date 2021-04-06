46e5b780342fdc399773806fd45e8e625f4c57df
Josue <reyesjosue0@gmail.com>
2021-04-06 19:45:31 +0200

    Database

---
+++ Database/Database.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Database {
+
+    public static ArrayList<Student> Studentenlijst = new ArrayList<>();
+    public static ArrayList<Examinator> Examinatorlijst = new ArrayList<>();
+    public static ArrayList<Exam> alleExamens = new ArrayList<>();
+    public static ArrayList<Student> AutoExamenDeelnemers = new ArrayList<>();
+    public static ArrayList<Student> VaarExamenDeelnemers = new ArrayList<>();
+
+}
